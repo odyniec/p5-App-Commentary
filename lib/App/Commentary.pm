bd119765ec6559f58ec1d96545534010a1c7c864
Michal Wojciechowski <odyniec@cpan.org>
2014-08-12 19:44:22 +0200

    Load index.html for the root path

--- lib/App/Commentary.pm
+++ lib/App/Commentary.pm
@@ -10,4 +10,10 @@ use warnings;
 use Dancer ':syntax';
 use Dancer::Plugin::Commentary;
 
+get '/' => sub {
+    # TODO: We may want to make this configurable, in case some static site 
+    # generator doesn't do index.html
+    forward '/index.html';
+};
+
 1;
